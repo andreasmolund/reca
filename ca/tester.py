9d783a6a91eea681479d11c8e2b3817efed57c72
Andreas Molund <andreas.molund@gmail.com>
2016-10-12 12:02:08 +0200

    Runnable from terminal

--- ca/tester.py
+++ ca/tester.py
@@ -1,18 +1,31 @@
+import sys
+import getopt
 from ca import CA
 import numpy as np
 import util
 import math
 
 
-size = 101
-steps = int(math.ceil(size / 2))
+length = 21
+rule = 90
+simple = True
 
 
-def test():
+def test(raw_args):
     """Internal test for the CA"""
-    init_config = util.init_config_simple(size)
-    rule = 90
 
+    # length, simple, rule
+    opts, args = getopt.getopt(raw_args[1:], "l:s:r:")
+    for o, a in opts:
+        if o == '-l':
+            length = int(a)
+        elif o == '-s':
+            simple = a in ['True', 'true', 'y']
+        elif o == '-r':
+            rule = int(a)
+
+    steps = int(math.ceil(length / 2))
+    init_config = util.init_config_simple(length) if simple else util.init_config_rand(length)
     automation = CA(1, rule, np.asarray(init_config), steps)
 
     for t in xrange(steps):
@@ -22,5 +35,5 @@ def test():
 
 
 if __name__ == '__main__':
-    test()
+    test(sys.argv)
 
