6dd08cfdb6a0db2853ad2e6a88d706b4349bd517
Andreas Molund <andreas.molund@gmail.com>
2017-04-28 10:26:34 +0200

    Starting to finish before experiment running

--- jobscripts/mytestjob.sh
+++
@@ -1,7 +0,0 @@
-#PBS -N andrmolutestjob
-#PBS -A andrmolu
-#PBS -l select=2:ncpus=1:mpiprocs=1:mem=100mb
-#PBS -l walltime=0:01:00
-
-module load python/2.7.3
-python /home/andrmolu/supercomp/testpyscript.py
